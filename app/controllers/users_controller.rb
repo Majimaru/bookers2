9d30f8b69f6ca0a0ed87e001f86a6c6ed37c03de
Majimaru <141239403+Majimaru@users.noreply.github.com>
2023-08-19 11:54:58 +0900

    [Break] 理由：私用のため, 作業中タスク：ユーザー詳細画面の実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   
+  get 'books/index'
+  get 'books/show'
   root to: 'homes#top'
   devise_for :users
   
   get '/homes/about' => 'homes#about', as: 'about'
+  
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:index, :show, :create]
   
 end
