206dd08f8c40132fb8009adac63aa15a65441320
Majimaru <141239403+Majimaru@users.noreply.github.com>
2023-08-19 23:47:14 +0900

    [Add] ユーザー情報編集画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,16 +12,26 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user)
   end
   
   private
   
   def is_matching_login_user
     user = User.find(params[:id])
-    
     unless user.id == current_user.id
       redirect_to user_path(user)
     end
+  end
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
   
 end
