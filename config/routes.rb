b676889057bea9f6b5d758ceda8b859cc425b9b1
Majimaru <141239403+Majimaru@users.noreply.github.com>
2023-08-21 06:36:23 +0900

    [Add] アクセス権限, バリデーションの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   
+  root to: 'homes#top'
+  
   get 'books/index'
   get 'books/show'
-  root to: 'homes#top'
-  devise_for :users
-  
   get '/homes/about' => 'homes#about', as: 'about'
+  
+  devise_for :users
   
   resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show, :edit, :create, :update, :destroy]
