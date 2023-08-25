c3632fc85dc775e0875078d3b6082e56a28753b3
Majimaru <141239403+Majimaru@users.noreply.github.com>
2023-08-25 18:24:20 +0900

    [Start] 応用課題3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -40,7 +40,7 @@ class BooksController < ApplicationController
       flash[:notice] = "You have updated book successfully."
       redirect_to book_path(@book)
     else
-      render :edit
+      render :edit5
     end
   end
   
